898fa89e639181c9a01bf653ada20b264bfe14b9
EtoileFoula <gillet.valentin45000@gmail.com>
2022-05-05 18:17:26 +0200

    Training 3

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-let mass = 0;
-let fat_rate = 0;
-let activity_rate = 0;
+let a_mass = 0;
+let a_fat_rate = 0;
+let a_activity_rate = 0;
 let obj_fat = 0;
 let obj_lean = 0;
 let maintain_cal = 0;
 let obj_fat_cal = 0;
 let obj_lean_cal = 0;
 
-mass = prompt("entrez votre poids");
-fat_rate = prompt("entrez votre tx de graisse");
-activity_rate = prompt("entrez votre coef activité");
+a_mass = prompt("entrez votre poids");
+a_fat_rate = prompt("entrez votre tx de graisse");
+a_activity_rate = prompt("entrez votre coef activité");
+
+b_mass = parseInt(a_mass);
+b_fat_rate = parseInt(a_fat_rate);
+b_activity_rate = parseInt(a_activity_rate);
 
 function maintain() {
-  maintain_cal = (370 + 21.6 * (1 - fat_rate) * mass) * activity_rate;
+  maintain_cal =
+    (370 + 21.6 * (1 - b_fat_rate / 100) * b_mass) * b_activity_rate;
   return maintain_cal;
 }
+
+maintain();
+
+console.log(maintain_cal);
